cf308d6a9584ca7db4ec69b474f8888bf4418148
ultracold <ultracold273@users.noreply.github.com>
2024-12-11 13:22:27 +0800

    Update Deploy.ps1 and validate the inputs

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,148 +1,185 @@
+$TOML_FILE = "config.toml"
 
-$DirectoryId = "your_directory_id"
-$SubscriptionId = "your_subscription_id"
-
-$ResourceGroupName = "your_resource_id"
-$Location = "southeastasia"
-$TemplateFilePath = ".\linux.bicep"
-$vmName = "your_vm_name"
-$adminUserName = "your_admin_username"
-$adminPassword = "your_admin_password"
-
-$publicKey = $null
-
-function DeployTemplateAzCli {
-    param
-    (
-        [Parameter(Mandatory = $true)]
-        [string]$DirectoryId,
-
-        [Parameter(Mandatory = $true)]
-        [string]$SubscriptionId,
-
-        [Parameter(Mandatory = $true)]
-        [string]$ResourceGroupName,
-
-        [Parameter(Mandatory = $true)]
-        [string]$Location,
-
-        [Parameter(Mandatory = $true)]
-        [string]$TemplateFilePath,
-
-        [Parameter(Mandatory = $true)]
-        [object]$TemplateParameter
+function ConvertFrom-Toml {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        [string]$line
     )
 
-    az login --tenant $DirectoryId
-    az account set --subscription $SubscriptionId
-
-    # test if the resource group exists
-    $resourceGroup = az group show --name $ResourceGroupName --query "name" --output tsv 2>$null
-    if (-not $resourceGroup) {
-        Write-Host "Resource Group $ResourceGroupName does not exist. Creating it ..."
-        az group create --name $ResourceGroupName --location $Location --output none
+    begin {
+        $result = @{}
+        $regex = '([\w\-]+)\s*=\s*(".*"|\d+|true|false|)'
     }
 
-    # deploy the template
-    $parameterList = $TemplateParameter.GetEnumerator() | ForEach-Object {
-        if ($null -ne $_.Value) {
-            "$($_.Key)=`"$($_.Value)`""
-        } else {
-            "$($_.Key)=`"`""
+    process {
+        $line = $line.Trim()
+
+        # Skip empty or comment line
+        if ($line -eq '' -or $line -match '^\s*#') {
+            continue
+        }
+
+        if ($line -match $regex) {
+            $key = $matches[1]
+            $value = $matches[2]
+
+            if ($value -match '^".*"$') {
+                $value = $value.Trim('"')
+            }
+            elseif ($value -match '^\d+$') {
+                $value = [int]$value
+            }
+            elseif ($value -match '^true$|^false$') {
+                $value = [bool]$value
+            }
+
+            $result[$key] = $value
         }
     }
 
-    $deploymentOutput = $(az deployment group create `
-        --resource-group $ResourceGroupName `
-        --template-file $TemplateFilePath `
-        --parameters $parameterList `
-        --output json)
-
-    return $deploymentOutput | ConvertFrom-Json
+    end {
+        $result
+    }
 }
 
-function Check-Password {
+$configs = Get-Content $TOML_FILE | ConvertFrom-Toml
+
+$validKeys = @("DIRECTORY_ID", "SUBSCRIPTION_ID", "RESOURCE_GROUP_NAME", "LOCATION", "VM_NAME", "ADMIN_USERNAME", "ADMIN_PASSWORD")
+
+$validKeys | ForEach-Object {
+    if (-not $configs.ContainsKey($_)) {
+        Write-Host "Missing key $_ in $TOML_FILE"
+        exit 1
+    }
+}
+
+Write-Host "Get the configuration from $TOML_FILE"
+
+$DirectoryId = $configs["DIRECTORY_ID"]
+$SubscriptionId = $configs["SUBSCRIPTION_ID"]
+$ResourceGroupName = $configs["RESOURCE_GROUP_NAME"]
+$Location = $configs["LOCATION"]
+$VmName = $configs["VM_NAME"]
+$AdminUsername = $configs["ADMIN_USERNAME"]
+$AdminPassword = $configs["ADMIN_PASSWORD"]
+$TemplateFilePath = "linux2.bicep"
+
+Write-Host "DirectoryId: $DirectoryId"
+Write-Host "SubscriptionId: $SubscriptionId"
+Write-Host "ResourceGroupName: $ResourceGroupName"
+Write-Host "Location: $Location"
+Write-Host "VM Name: $VmName"
+Write-Host "Admin Username: $AdminUsername"
+Write-Host "Admin Password: $AdminPassword"
+
+function Confirm-VmName {
     param (
-        [string]$input
+        [Parameter(Mandatory = $true)]
+        [string]$VmName
     )
 
-    $length = $input.Length
-    if ($length -lt 6 -or $length -gt 72) {
-        Write-Host "字符串长度必须在6到72之间。"
-        return $false
+    if ($VmName.Length -lt 1 -or $VmName.Length -gt 64) {
+        Write-Host "The VM name must be 1-64 characters long."
+        exit 1
     }
 
-    # 定义条件
-    $hasUppercase = $input -match '[A-Z]'     # 大写字符
-    $hasLowercase = $input -match '[a-z]'     # 小写字符
-    $hasDigit = $input -match '[0-9]'         # 数字
-    $hasSpecial = $input -match '[^a-zA-Z0-9]' # 特殊字符
+    if ($VmName -cnotmatch "^[a-zA-Z0-9]([-a-zA-Z0-9]{0,62})[a-zA-Z0-9]$") {
+        Write-Host "The VM name must contain only letters and digits or hypen and cannot start or end with hypens"
+        exit 1
+    }
 
-    # 计分变量
+    Write-Host "VM name $VmName is valid."
+}
+
+function Confirm-Password {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Passkey
+    )
+
+    if ($Passkey.Length -lt 6 -or $Passkey.Length -gt 72) {
+        Write-Host "The password must be at least 12 characters and at most 72 characters long."
+        exit 1
+    }
+
     $score = 0
+    if ($Passkey -match "[A-Z]") { $score++ }
+    if ($Passkey -match "[a-z]") { $score++ }
+    if ($Passkey -match "[0-9]") { $score++ }
+    if ($Passkey -match "[^a-zA-Z0-9]") { $score++ }
 
-    # 检查每个条件
-    if ($hasUppercase) { $score++ }
-    if ($hasLowercase) { $score++ }
-    if ($hasDigit) { $score++ }
-    if ($hasSpecial) { $score++ }
-
-    # 判断满足的条件数
-    if ($score -ge 3) {
-        return $true
-    } else {
-        Write-Host "Password does not satisfy the requirements."
-        return $false
-    }
-}
-
-if ([string]::IsNullOrEmpty($adminPassword)) {
-    Write-Host "Generating a new SSH key pair ..."
-    ssh-keygen.exe -t rsa -b 2048 -m PEM -f $vmName -N ""
-
-    # test if the public and private file exists
-    if (-not (Test-Path -Path "$vmName")) {
-        Write-Host "Private key file $vmName does not exist. Exiting ..."
-        exit
-    }
-    if (-not (Test-Path -Path "$vmName.pub")) {
-        Write-Host "Public key file $vmName.pub does not exist. Exiting ..."
-        exit
+    if ($score -lt 3) {
+        Write-Host "The password must contain at least three of the following: uppercase letter, lowercase letter, number, and special character."
+        exit 1
     }
 
-    # read the public key file
-    $publicKey = Get-Content -Path "$vmName.pub"
+    Write-Host "Password $Passkey is valid."
 }
 
-if ([string]::IsNullOrEmpty($publicKey) -and [string]::IsNullOrEmpty($adminPassword)) {
-    Write-Host "Public key and admin password are both empty. Exiting ..."
-    exit
+Confirm-VmName -VmName $VmName
+
+Confirm-Password -Passkey $AdminPassword
+
+az login --tenant $DirectoryId # --use-device-code
+az account set --subscription $SubscriptionId
+
+$resourceGroup = az group show --name $ResourceGroupName --query "name" --output json 2>$null
+if (-not $resourceGroup) {
+    Write-Host "Resource Group $ResourceGroupName does not exist. Creating it ..."
+    az group create --name $ResourceGroupName --location $Location --output none
 }
 
-$parameter = @{
-    pLocation      = $Location
-    pVmName        = $vmName
-    pAdminUsername = $adminUserName
-    pAdminPassword = $adminPassword
-    pSshPublicKey  = $publicKey
-}
-
-$deployment = DeployTemplateAzCli -DirectoryId $DirectoryId -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -Location $Location -TemplateFilePath $TemplateFilePath -TemplateParameter $parameter
-
-$ipAddress = $deployment.properties.outputs.ipAddress.value
-$hostname = $deployment.properties.outputs.hostname.value
-
-Write-Host "The VM $vmName has been created with IP address $ipAddress and hostname $hostname."
-if ([string]::IsNullOrEmpty($adminPassword)) {
-    Write-Host "you can now use `ssh -i $vmName $adminUserName@$hostname` to connect to the VM."
-} else {
-    Write-Host "you can now use `ssh $adminUserName@$hostname` to connect to the VM."
-}
-
-$trojanAddress="https://raw.githubusercontent.com/ultracold273/deploy_azure/refs/heads/main/trojan.sh"
-
-$commandOutput = $(az vm run-command invoke `
+$deploymentOutput = az deployment group create `
     --resource-group $ResourceGroupName `
-    --name $vmName `
+    --template-file $TemplateFilePath `
+    --parameters `
+        pLocation=$Location `
+        pVmName=$VmName `
+        pAdminUsername=$AdminUsername `
+        pAdminPassword=$AdminPassword `
+        pSshPublicKey= `
+    --output json | Tee-Object -Variable deploymentOutput
+
+$deploymentOutput = $deploymentOutput | ConvertFrom-Json
+
+$DeploymentResult = $deploymentOutput.properties.provisioningState
+
+if ($DeploymentResult -ne "Succeeded") {
+    Write-Host "Deployment failed."
+    exit 1
+}
+
+Write-Host "Deployment Result: $DeploymentResult"
+
+$IpAddress = $deploymentOutput.properties.outputs.ipAddress.value
+$Hostname = $deploymentOutput.properties.outputs.hostname.value
+
+if ($null -eq $IpAddress -or $null -eq $Hostname) {
+    Write-Host "Failed to get the IP address or hostname. Exit.."
+    exit 1
+}
+
+Write-Host "IP Address: $IpAddress"
+Write-Host "Hostname: $Hostname"
+
+$trojanAddress = "https://raw.githubusercontent.com/ultracold273/deploy_azure/refs/heads/main/trojan.sh"
+
+$commandOutput = az vm run-command invoke `
+    --resource-group $ResourceGroupName `
+    --name $VmName `
     --command-id RunShellScript `
-    --scripts "curl -s $trojanAddress | bash -s -- $hostname $ipAddress")
+    --scripts "curl -s $trojanAddress | bash -s -- $Hostname $IpAddress" | Tee-Object -Variable commandOutput
+
+$commandOutput = $commandOutput | ConvertFrom-Json
+$Message = $commandOutput.value[0].message
+
+if ($Message -match "Now you can setup your client with passcode: (\w+) or (\w+)") {
+    $passcode1 = $matches[1]
+    $passcode2 = $matches[2]
+    Write-Host "Passcode 1: $passcode1"
+    Write-Host "Passcode 2: $passcode2"
+} else {
+    Write-Host "Failed to get the passcode. Exit.."
+    exit 1
+}
