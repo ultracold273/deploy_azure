31332e406a0a0aab2f6251d2ce6586e48fa18143
Ubuntu <lxwei@claw-sea.ttemovc3abye5o3pg2tyumka1h.ix.internal.cloudapp.net>
2026-05-08 09:57:46 +0000

    Address review feedback on diff size and nginx comments

--- setup.sh
+++ setup.sh
@@ -24,14 +24,14 @@ install_deps() {
 configure_nginx_hash() {
     NGINX_CONF=/etc/nginx/nginx.conf
 
-    if grep -q "server_names_hash_bucket_size" "$NGINX_CONF"; then
-        sed -i 's/^\s*server_names_hash_bucket_size.*/    server_names_hash_bucket_size 128;/' "$NGINX_CONF"
+    if grep -Eq "^[[:space:]]*#?[[:space:]]*server_names_hash_bucket_size" "$NGINX_CONF"; then
+        sed -i 's/^[[:space:]]*#\?[[:space:]]*server_names_hash_bucket_size.*/    server_names_hash_bucket_size 128;/' "$NGINX_CONF"
     else
         sed -i '/http {/a\    server_names_hash_bucket_size 128;' "$NGINX_CONF"
     fi
 
-    if grep -q "server_names_hash_max_size" "$NGINX_CONF"; then
-        sed -i 's/^\s*server_names_hash_max_size.*/    server_names_hash_max_size 1024;/' "$NGINX_CONF"
+    if grep -Eq "^[[:space:]]*#?[[:space:]]*server_names_hash_max_size" "$NGINX_CONF"; then
+        sed -i 's/^[[:space:]]*#\?[[:space:]]*server_names_hash_max_size.*/    server_names_hash_max_size 1024;/' "$NGINX_CONF"
     else
         sed -i '/http {/a\    server_names_hash_max_size 1024;' "$NGINX_CONF"
     fi
